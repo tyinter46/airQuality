5f520ea5534629e8c45c3d8c7e69260abd854656
tyinter46 <tyinter46@example.com>
2022-09-09 07:05:10 +0100

    posted after one minute

--- server/utils/scheduler.ts
+++ server/utils/scheduler.ts
@@ -7,6 +7,7 @@ export const postEveryMinute = ()=>{
 nodeCron.schedule('* * * * *', ()=>{
    // console.log('i am running every minute')
    postAirQuaity()
+   console.log('posted after one minute')
 })
 }
 
